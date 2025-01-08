e289771a4b4f8aeea9b0a0d99d8a769835b32401
DanielADelRosario <danieladelrosario@iskolarngbayan.pup.edu.ph>
2025-01-08 22:53:28 +0800

    db updates

--- database/migrations/2024_12_19_095125_create_company_table.php
+++ database/migrations/2024_12_19_095125_create_company_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('company', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); 
+            $table->string('company_name'); 
             $table->string('contact_person');
             $table->string('contact_number');  
             $table->string('email')->unique();
