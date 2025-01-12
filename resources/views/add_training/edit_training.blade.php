00d8bdc611ee2940a28bb36664f6c52d83a0e011
R0ck0t0 <145852340+R0ck0t0@users.noreply.github.com>
2025-01-13 02:22:56 +0800

    Sweet alert template for edit training

--- resources/views/add_training/edit_training.blade.php
+++ resources/views/add_training/edit_training.blade.php
@@ -8,7 +8,7 @@
             <!-- Card -->
             <div class="card shadow-sm rounded-3">
                 <!-- Title -->
-                <div class="d-flex justify-content-center align-items-center mb-0 bg-primary rounded-top h-80px" >
+                <div class="d-flex justify-content-center align-items-center mb-0 bg-primary rounded-top h-80px">
                     <h2 class="text-white fw-boldest m-0 fs-1">EDIT TRAINING</h2>
                 </div>
                 <!-- Form -->
@@ -60,19 +60,23 @@
 
                         <!-- Email and Password -->
                         <div class="row mb-4" id="credentials-container">
-                            <div class="flex-grow-1">
+                            <div class="col-md-6">
                                 <label for="credentials" class="fw-bold mb-2 required">Account</label>
                                 <select id="credentials" class="form-select form-select-solid">
                                     <option value="" disabled selected>Select Account to Host Training</option>
                                     <option value="samplezoomaccountpassword">alpszoomaccount1@gmail.com</option>
                                 </select>
                             </div>
+
+                            <div class="col-md-6">
+                                <label for="platform" class="fw-bold mb-2">Platform</label>
+                                <input type="text" name="platform" id="platform" class="form-control form-control-solid" placeholder="Enter Platform (e.g. Zoom)">
+                            </div>
                         </div>
                         <!-- Location: Face-to-Face and In-Person -->
                         <div class="mb-4 d-none" id="location-container">
                             <label for="location" class="fw-bold mb-2 required">Location </label>
-                            <input type="text" id="location" class="form-control form-control-solid"
-                                placeholder="Enter Location">
+                            <input type="text" id="location" class="form-control form-control-solid" placeholder="Enter Location">
                         </div>
                     </form>
                     <div class="row mb-4" id="company-course-container">
@@ -113,7 +117,7 @@
                                 <input type="text" id="date-range" class="form-control form-control-solid pe-5">
                                 <span class="position-absolute top-50 end-0 translate-middle-y me-3 text-muted">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-heart-fill" viewBox="0 0 16 16">
-                                        <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+                                        <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
                                     </svg>
                                 </span>
                             </div>
@@ -129,36 +133,57 @@
                     </div>
 
                     <!-- Facilitator and Assistant -->
-                    <div class="row mb-5">
-                        {{-- need form repeater --}}
-                        <div class="col-md-6 d-flex align-items-lg-end">
-                            <div class="flex-grow-1">
-                                <label for="facilitator" class="fw-bold mb-2 required">Facilitator</label>
-                                <select id="facilitator" class="form-select form-select-solid">
-                                    <option disabled selected>Select Facilitator</option>
-                                    <option value="">No Facilitator Yet</option>
-
-                                </select>
-                            </div>
+                    <div class="row mb-5 align-items-start">
+                        <!-- Facilitator -->
+                        <div class="col-md-6">
+                            <label for="facilitator" class="fw-bold mb-2 required">Facilitator</label>
+                            <select id="facilitator" class="form-select form-select-solid">
+                                <option disabled selected>Select Facilitator</option>
+                                <option value="">No Facilitator Yet</option>
+                                {{-- @foreach ($users as $user)
+                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                @endforeach --}}
+                            </select>
                         </div>
+                        <!-- Assistant -->
+                        <div class="col-md-6">
+                            <!-- Repeater -->
+                            <div id="asst_repeat">
+                                <div class="form-group">
+                                    <!-- Label for the Assistant field -->
+                                    <label class="form-label">Assistant:</label>
 
-                        {{-- need form repeater --}}
-                        <div class="col-md-6 d-flex align-items-lg-end">
-                            <div class="flex-grow-1">
-                                <label for="assistant" class="fw-bold mb-2 required">Assistant </label>
-                                <select id="assistant" class="form-select form-select-solid">
-                                    <option disabled selected>Select Assistant</option>
-                                    <option value="">No Assistant Yet</option>
-
-                                </select>
+                                    <!-- Repeater List -->
+                                    <div data-repeater-list="asst_repeat">
+                                        <div data-repeater-item>
+                                            <div class="form-group row align-items-center">
+                                                <div class="col-md-9">
+                                                    <input type="text" class="form-control form-control-solid mb-3 assistant" id="assistant" placeholder="Enter Assistant's Name" />
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <a href="javascript:;" data-repeater-delete class="btn btn-sm btn-light-danger mb-3">
+                                                        <i class="la la-trash-o"></i> DELETE
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Add Button -->
+                                <div class="form-group mt-3">
+                                    <a href="javascript:;" data-repeater-create class="btn btn-light-primary btn-sm">
+                                        <i class="la la-plus"></i> ADD
+                                    </a>
+                                </div>
                             </div>
+                            <!-- End Repeater -->
                         </div>
                     </div>
 
                     <!-- Buttons -->
                     <div class="d-flex justify-content-center gap-5 ">
-                        <a href="{{route ('calendar')}}">
-                            <button type="button" class="btn btn-light fw-boldest">CANCEL</button>
+                        <a href="{{ route('calendar') }}">
+                            <button type="button" id="cancel_training_button" class="btn btn-light fw-boldest">CANCEL</button>
                         </a>
                         <button type="button" id="edit_training_submit" class="btn btn-success fw-boldest">SAVE</button>
                     </div>
@@ -170,5 +195,8 @@
 @endsection
 @push('scripts')
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    {{-- <script src="{{ asset('js/edit_training.js') }}"></script> --}}
+    <script src="{{ asset('js/edit_training.js') }}"></script>
+    <script src="{{ asset('plugins/custom/formrepeater/formrepeater.bundle.js') }}"></script>
+
+
 @endpush
