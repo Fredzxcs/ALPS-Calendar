cdc82416228a62e2c918c451eda71cc6e5436bb0
John Loyd Cabral <145852340+R0ck0t0@users.noreply.github.com>
2025-01-17 11:13:41 +0800

    Bug fix on validation

--- public/js/edit_training.js
+++ public/js/edit_training.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     let startDateFormatted = '';
     let endDateFormatted = '';
 
