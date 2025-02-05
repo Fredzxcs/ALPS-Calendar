d7751f8273313d20d118c2608d1126608affd97a
Kimberly Mae Kho <kimlykho27@gmail.com>
2025-02-05 10:40:10 +0800

    icon on tab

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ALPs Calendar</title>
+    <link rel="icon" href="{{ asset('img/ALPs_Logo.png') }}" type="image/x-icon">
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
